10346eb011b659f449d5df99f095a449024e4226
Mohamud Olow <khalif18@live.com>
2019-09-10 22:36:53 -0700

    create a box at a random index after a set interval of time

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const gameScope = document.getElementById('gameScope');
 const box = "box";
+const gameScopeArea = gameScope.offsetWidth * gameScope.offsetHeight;
 
+const boxes = gameScopeArea/400; // the box is 20px by 20px in size, area is 400 each
 
-makeBox();
-function makeBox() {
+for(var i=0; i<boxes; i++) {
     const gameBox = document.createElement('div');
     gameBox.classList.add(box);
     gameScope.append(gameBox);
 }
+
+const gameBoxes = document.querySelectorAll(`.${box}`);
+
+const  randomBox = () => {
+    const random = Math.floor(Math.random() * (gameBoxes.length - 40));
+    gameBoxes[random].classList.add("purple");
+}
+
+setInterval(randomBox, 10000);

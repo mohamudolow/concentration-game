b2d353d2977bfde183b754565f69141d8450b1fe
Mohamud Olow <khalif18@live.com>
2019-09-11 01:06:36 -0700

    add event listeners to play & stop buttons

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const gameScope = document.getElementById('gameScope');
 const box = "box";
 const gameScopeArea = gameScope.offsetWidth * gameScope.offsetHeight;
 
-const boxes = gameScopeArea/400; // the box is 20px by 20px in size, area is 400 each
+const boxes = gameScopeArea/1600; // the box is 40px by 40px in size, area is 400 each
+
+const playButton = document.querySelector(".fa-play-circle");
+const pauseButton = document.querySelector(".fa-pause-circle");
 
 for(var i=0; i<boxes; i++) {
     const gameBox = document.createElement('div');
@@ -13,8 +16,24 @@ for(var i=0; i<boxes; i++) {
 const gameBoxes = document.querySelectorAll(`.${box}`);
 
 const  randomBox = () => {
-    const random = Math.floor(Math.random() * (gameBoxes.length - 40));
+    const random = Math.floor(Math.random() * (gameBoxes.length - 20));
     gameBoxes[random].classList.add("purple");
 }
 
-setInterval(randomBox, 10000);
+const hideBox = () => {
+    gameBoxes.forEach((clearBox) => {
+        clearBox.classList.remove("purple");
+    });
+}
+
+var randomBoxInterval;
+var hideBoxInterval;
+playButton.addEventListener("click", function() {
+    randomBoxInterval = setInterval(randomBox, 1000);
+    hideBoxInterval = setInterval(hideBox, 1050);
+});
+
+pauseButton.addEventListener("click", function() {
+    clearInterval(randomBoxInterval);
+    clearInterval(hideBoxInterval);
+});

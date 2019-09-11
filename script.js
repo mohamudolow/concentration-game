a395fc234d60f140597dc701c035b2468ed82b08
Mohamud Olow <khalif18@live.com>
2019-09-11 06:46:31 -0700

    set conditions for incrementing or decrementing counters & determine if player wins or not

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const gameScope = document.getElementById('gameScope');
 const box = "box";
 const gameScopeArea = gameScope.offsetWidth * gameScope.offsetHeight;
 
-const boxes = gameScopeArea/1600; // the box is 40px by 40px in size, area is 400 each
+const boxes = gameScopeArea/1600; // the box is 40px by 40px in size, area is 1600
 
 const playButton = document.querySelector(".fa-play-circle");
 const pauseButton = document.querySelector(".fa-pause-circle");
+var score = document.getElementById("score");
+var countdown = document.getElementById("count");
 
+//create boxes
 for(var i=0; i<boxes; i++) {
     const gameBox = document.createElement('div');
     gameBox.classList.add(box);
@@ -15,11 +18,46 @@ for(var i=0; i<boxes; i++) {
 
 const gameBoxes = document.querySelectorAll(`.${box}`);
 
+var counter = 50;
+
+// generate random box & determine if player wins or not
 const  randomBox = () => {
     const random = Math.floor(Math.random() * (gameBoxes.length - 20));
     gameBoxes[random].classList.add("purple");
+    
+    if(counter > 0) {
+        counter--;
+    } else if(score.innerHTML > 25) {
+        alert(`Game over. You won!
+        Your score is: ${score.innerHTML}/50.`);
+        location.reload();
+    }  else if(score.innerHTML < 25){
+        alert(`Game over. You lost! 
+Your score is: ${score.innerHTML}/50. 
+Try again!`);
+        location.reload();
+    }  else {
+        alert(`Game over. It's a draw!
+        Your score is: ${score.innerHTML}/50. You can try again to win!`);
+        location.reload();
+    }
+
+    countdown.innerHTML = counter; 
 }
 
+// game score
+gameBoxes.forEach((gamebox) => {
+    gamebox.addEventListener("click", function() {
+        if(gamebox.classList.contains("purple")) {
+            gamebox.classList.remove("purple");
+            score.innerHTML = parseFloat(score.innerHTML) + 1;
+        } else {
+            score.innerHTML = parseFloat(score.innerHTML) + 0;
+        }
+});
+                  });
+
+//hide box after some time 
 const hideBox = () => {
     gameBoxes.forEach((clearBox) => {
         clearBox.classList.remove("purple");
@@ -29,8 +67,8 @@ const hideBox = () => {
 var randomBoxInterval;
 var hideBoxInterval;
 playButton.addEventListener("click", function() {
-    randomBoxInterval = setInterval(randomBox, 1000);
-    hideBoxInterval = setInterval(hideBox, 1050);
+    randomBoxInterval = setInterval(randomBox, 1500);
+    hideBoxInterval = setInterval(hideBox, 1600);
 });
 
 pauseButton.addEventListener("click", function() {
